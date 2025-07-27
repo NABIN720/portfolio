7eb4ae5636965996fa9ff5d5ee4a1f48b83da01a
NABIN720 <nabinbhattarai720@gmail.com>
2025-07-27 11:36:52 +0545

    added procfile updated wsgi build.sh

--- portfolio/portfolio/wsgi.py
+++ portfolio/portfolio/wsgi.py
@@ -2,16 +2,8 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Add the project directory to Python path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Set Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings_vercel')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
-
-# Vercel serverless function handler
-def handler(request, context):
-    return application(request, context)
-
-app = application
