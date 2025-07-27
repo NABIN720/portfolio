3538713e75d4c84e3547b77514cc639a6f5ee508
NABIN720 <nabinbhattarai720@gmail.com>
2025-07-27 13:20:36 +0545

    uupdated settings for media

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('api/', include('api_services.urls')),
 ] 
 
-if settings.DEBUG:
+if not settings.DEBUG:  # Even in production
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
