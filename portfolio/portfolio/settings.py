32a6703a92704e49ad9c1edcab72d03946e0efda
NABIN720 <nabinbhattarai720@gmail.com>
2025-07-27 21:04:41 +0545

    copied media files as static files

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-# DEBUG = True 
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True 
 ALLOWED_HOSTS = ['*']
 
 
