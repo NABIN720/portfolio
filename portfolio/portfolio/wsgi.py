b3b312c4c0c9c0b0e2a2f427a63da65e7faa7e49
NABIN720 <nabinbhattarai720@gmail.com>
2025-07-27 10:01:54 +0545

    added json

--- portfolio/portfolio/wsgi.py
+++ portfolio/portfolio/wsgi.py
@@ -6,7 +6,7 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Set Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_site.settings_vercel')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings_vercel')
 
 application = get_wsgi_application()
 
