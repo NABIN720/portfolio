0f1ff39e06fe8806d94f71a7530ce1d6d6d58158
NABIN720 <nabinbhattarai720@gmail.com>
2025-08-06 17:57:58 +0545

    beautifying

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -124,13 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
+STATIC_URL = "/static/" 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build","static")
-STATIC_URL = "/staticfiles/" 
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
