befde4cb9b2a1bf5986cddcb1a2358fa6a7d20d7
NABIN720 <nabinbhattarai720@gmail.com>
2025-08-07 11:16:02 +0545

    udpated main

---
+++ portfolio/wsgi.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+
+# Set Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+
+application = get_wsgi_application()
+app = application
