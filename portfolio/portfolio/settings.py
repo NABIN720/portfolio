d019c48e071795d4111ea90f64c17b321961f307
NABIN720 <nabinbhattarai720@gmail.com>
2025-08-07 09:14:32 +0545

    updated hosts

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = True 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
