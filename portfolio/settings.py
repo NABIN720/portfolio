e2481059c522782e32388b55b6744766f7532eff
NABIN720 <nabinbhattarai720@gmail.com>
2025-08-12 13:09:17 +0545

    added the custom domain in the settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = True 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['nabinbhattarai021.com.np', 'www.nabinbhattarai021.com.np']
 
 
 # Application definition
