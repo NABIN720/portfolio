36ab89793d74164877a958066a748930b84d7283
NABIN720 <nabinbhattarai720@gmail.com>
2025-08-29 17:15:10 +0545

    tweaked in vercel.json

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-# pip install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic --noinput
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Create build directory if it doesn't exist
+mkdir -p staticfiles_build
+
+# Copy static files to build directory
+cp -r staticfiles/* staticfiles_build/
